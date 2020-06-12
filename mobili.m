0a6f0100ef0b6b4a91c84a66e5537a25c5b59f4e
ysudhansh <sudhanshyelishetty@gmail.com>
2020-06-12 21:57:34 +0530

    Modifying mobili.m; corrected errors

--- mobili.m
+++ mobili.m
@@ -1,21 +1,22 @@
 function B = mobili(seq, frm, id)
 
 K = [721.53,0,609.55;0,721.53,172.85;0,0,1];
+avgCarLength = 3.8600;
+avgCarWidth = 1.6362;
 avgCarHeight = 1.5208;
+dir = "left_colour_imgs/";
 h = avgCarHeight;
 n = [0; -1; 0];
-seq = [ 2,10,  4,  8, 2, 9]; %Sequences
-frm = [98, 1,197,126,90,42]; %Frames
-id  = [ 1, 0, 20, 12, 1, 1]; %CarID's
 [tracklets_data, ground_truth] = tracklets(seq, frm, id);
 B = [];
 for i=1:size(tracklets_data,1)
-    b = [(tracklets_data(i,4) + tracklets_data(i,6))/2; (tracklets_data(i,5) + tracklets_data(i,7))/2; 1];
+    b = [(tracklets_data(i,4) + tracklets_data(i,6))/2; tracklets_data(i,7); 1];
     op = (-h * inv(K) * b) ./ (n' * inv(K) * b);
+    op = op + [0; avgCarHeight/2; avgCarLength/2];
     B = [B; tracklets_data(i,1) tracklets_data(i,2) tracklets_data(i,3) op'];
 end
 
-abs(ground_truth - B)
+error = abs(ground_truth - B) % Ignore 1st three columns, last three columns are all that matter.
 
 end
 
