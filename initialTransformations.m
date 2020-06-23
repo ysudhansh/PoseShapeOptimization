8cf39aadc152d1d9f80a87456ddeb4a0016141ec
ysudhansh <sudhanshyelishetty@gmail.com>
2020-06-23 15:49:07 +0530

    latest

--- initialTransformations.m
+++ initialTransformations.m
@@ -1,19 +1,12 @@
 function [transformed_coords, transformed_deformation_vectors] = initialTransformations()
 
-wireframe = scaleWireframe();
+[wireframe, deformation_vectors] = scaleWireframe();
 R = [-1 0 0; 0 0 -1; 0 -1 0]'; % World frame (world coordinate system) to camera frame (camera coordinate system)
-% R = -rotx(90);
-% t = [0.25*(wireframe(1,11) + wireframe(1,12) + wireframe(1,13) + wireframe(1,14)); 0.25*(wireframe(2,11) + wireframe(2,12) + wireframe(2,13) + wireframe(2,14)); 0.25*(wireframe(3,11) + wireframe(3,12) + wireframe(3,13) + wireframe(3,14))];
-t = [0; 0; 0];
 
-wireframe_hc = [wireframe; ones(1,14)];
-T = [R t];
-
-transformed_coords = T * wireframe_hc;
+transformed_coords = R * wireframe;
 % visualizeWireframe3D(transformed_coords);
 
 % Rotating deformation vectors
-deformation_vectors = importdata("vectors.txt");
 transformed_deformation_vectors = zeros(size(deformation_vectors));
 for i = 1:size(deformation_vectors,1)
     in = reshape(deformation_vectors(i,:),3,14);
